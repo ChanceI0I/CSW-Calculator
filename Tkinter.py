5f8826b2a5207807f26634dd0a9aec3e7a95ac20
Chance <Chengzhi-Yang@outlook.com>
2022-10-17 13:27:25 -0600

    0.2

--- Tkinter.py
+++ Tkinter.py
@@ -1,58 +1,65 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
 # from ctypes import windll                         #Apply when blur
 # windll.shcore.SetProcessDpiAwareness(1)
 
 window = tk.Tk()
 
-# window.resizable(False, False)
-window.attributes("-alpha", 1)  # window's transparency 
-window.attributes("-topmost", 1)  # Window stacking
-window.title("Tkinter")           # Title
-# window.geometry("270x350")        # size
-window.config(bg="white")         # Backgroung Colors
+window.resizable(False, False)
+window.attributes("-alpha", 1)     # window's transparency 
+window.attributes("-topmost", 1)   # Window stacking
+window.title("Calculator")         # Title
+# window.geometry("270x350")       # size
+# window.config(bg="blue")         # Backgroung Colors
+
+style = ttk.Style()
+style.configure("FrameMain", background="red")
+style.configure("FrameBtn", background="yellow")
+style.configure("Input", bg="black")
+style.theme_use("xpnative")
+style.configure("num", bg="purple")
+
 
 # window.columnconfigure(index=0, weight=1)
 # window.columnconfigure(index=1, weight=1)
 # window.columnconfigure(index=2, weight=1)
 # window.columnconfigure(index=3, weight=1)
+mainFrame = ttk.Frame(window, borderwidth=1, style="FrameMain.TFrame")
+mainFrame.grid(column=0, row=0)
 
-window.iconbitmap("C:/Users/Cheng/Desktop/Tkinter Calculator/calculator.ico")  # Set Icon
+btnFrame = ttk.Frame(mainFrame, style="FrameBtn.TFrame")
+btnFrame.grid(column=0, row=1)
 
-# mainFrame = Frame(window, borderwidth=1, bg="red")
-# mainFrame.grid()
-# mainFrame.rowconfigure(index=0, weight=1)
-# mainFrame.rowconfigure(index=1, weight=2)
+# window.iconbitmap("C:/Users/Cheng/Desktop/Tkinter Calculator/calculator.ico")  # Set Icon
+
 
 expression = StringVar()
 
-# canvas = Canvas(window, bd=0).grid_anchor(N)
-# inputField_Frame = Frame(window, borderwidth=2, bg="blue")
-# inputField_Frame.grid(column=0, row=0, columnspan=4)
-InputField = tk.Entry(
-    window, 
-    # width=17,
+InputField = ttk.Entry(
+    mainFrame, 
+    #width=13,
     # foreground="black", 
-    # background="white", 
+    # background="green", 
     textvariable=expression,
+    style="Input.TEntry"
     # font=('Helvetica 25'),
     )
 # InputField.config(highlightthickness=1, highlightbackground="black")
 # InputField.grid(column=0, row=0, columnspan=4)
-InputField.grid(column=0, row=0, columnspan=3, padx=5, pady=15)
+InputField.grid(column=0, row=0, columnspan=4)
 
-btnFrame = Frame(window)
-btnFrame.grid(column=0, row=1, columnspan=4, rowspan=5)
+
 
 for c in range(0, 4):
     for r in range(0, 4):
-        Button(btnFrame, text=c, width=10, height=4).grid(column=c, row=r, padx=2, pady=2)
+        ttk.Button(btnFrame, text=c, width=5, style="num.TButton").grid(column=c, row=r, padx=2, pady=5)
 
-# InputBtn_Frame = Frame(window).grid()
+# InputBtn_Frame = ttk.Frame(window).grid()
 
 
-
+# print(btnFrame.winfo_width(), InputField.winfo_width())
 
 
 window.mainloop()
